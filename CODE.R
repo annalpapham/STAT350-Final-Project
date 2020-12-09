25f560a9a327410a8eecedabb2fada4d21ca1f9e
Anna Pham <75589761+annalpapham@users.noreply.github.com>
2020-12-08 21:34:09 -0800

    Delete CODE.R

--- CODE.R
+++
@@ -1,9 +0,0 @@
-
-wd <- "/Users/PhuongAnh/STAT350-Project"
-setwd(wd)
-death = read.csv("death.csv")
-incident = read.csv("incd.csv")
-agesex = read.csv("agesex.csv")
-income = read.csv("income.csv")
-insurance = read.csv("insurance.csv")
-poverty = read.csv("poverty.csv")
